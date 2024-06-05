33f5ac8914dfac9cb159dbcc49b2502a5ddfa799
Laura <lauraalspaugh2002@icloud.com>
2024-06-05 19:15:36 -0400

    installing mdi icons

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='text-dark'>Notes</h1>
+        <button></button>
       </header>
     </div>
   );
