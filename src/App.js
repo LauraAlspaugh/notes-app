13eb6405ef64b60a22601ab4eff5ec73b5307cb7
Laura <lauraalspaugh2002@icloud.com>
2024-06-06 09:56:27 -0400

    layout

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className='text-dark'>Notes</h1>
-       <button className='add-button'><i className='mdi mdi-plus-thick fs-5'></i></button>
-      </header>
+     <div className='left-section display-flex justify-content-center'>
+        <h1 className=' note-text text-dark'>Notes</h1>
+       <button className='add-button' title='add a note'><i className='mdi mdi-plus-thick fs-5'></i></button>
+     </div>
+     <div className='right-section'>
+      <div className='preview'>
+<h3 className='write'>Write</h3>
+<h3 className='write'>Preview</h3>
+      </div>
+     </div>
+  
     </div>
   );
 }
