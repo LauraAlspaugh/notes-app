62cdff70b953079c00d7efa5d359deecfc2e9060
Laura <lauraalspaugh2002@icloud.com>
2024-06-06 10:27:13 -0400

    importing modals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
+ import Button from 'react-bootstrap/Button';
+ import Modal from 'react-bootstrap/Modal';
+
+
+
+
 
 
 function App() {
+  const [show, setShow] = React.useState(false);
+  
+  const handleClose = () => setShow( false);
+  const handleShow = () => setShow(true);
   return (
+    
     <div className="App">
      <div className='left-section display-flex justify-content-center'>
         <h1 className=' note-text text-dark'>Notes</h1>
@@ -16,6 +27,25 @@ function App() {
 <h3 className='write'>Preview</h3>
       </div>
      </div>
+
+  <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
   
     </div>
   );
